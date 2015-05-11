4ac02e138ba4cc2557bf1fa7cdc0d839fd21cca1
rwedoff <senor.rw@gmail.com>
2015-05-10 19:10:35 -0500

    Final Version of JS Lab 10 Tacobell Finder Web App

---
+++ lab10.3/js/ui-js.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('#list').hide();
+    $("#about").hide();
+    $("#list-but").on('click', function(){
+        $('#home').hide();
+  
+        $('#list').show();
+        
+    });
+      $("#home-but").on('click', function(){
+        $('#list').hide();
+  
+        $('#home').show();
+    });
+   
+  
+    
+});
